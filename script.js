2d227382e9de9a34ab80e74832f50bf91474bc9c
dgroh211 <dgroh211@gmail.com>
2023-03-27 14:20:08 -0400

    nav highlighted

--- script.js
+++ script.js
@@ -41,4 +41,21 @@ function myFunction() {
     });
   });
   aboutObserver.observe(aboutSection);
+
+  var navLinks = document.querySelectorAll(".nav-link");
+  var sections = document.querySelectorAll("section");
+
+  function highlightNavLink() {
+    var index = sections.length;
+
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+    navLinks.forEach((link) => link.classList.remove("active"));
+    navLinks[index].classList.add("active");
+  }
+
+  highlightNavLink();
+
+  window.addEventListener("scroll", highlightNavLink);
+
   
