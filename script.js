d8fb016aa63cecba202b491e2daa9543f8c595f8
dgroh211 <dgroh211@gmail.com>
2023-03-27 20:22:40 -0400

    added about us, no style, started faq

--- script.js
+++ script.js
@@ -58,4 +58,17 @@ function myFunction() {
 
   window.addEventListener("scroll", highlightNavLink);
 
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.question');
+    const answer = item.querySelector('.answer');
+  
+    question.addEventListener('click', () => {
+      answer.classList.toggle('show-answer');
+    });
+  });
+  
+  
+  
   
